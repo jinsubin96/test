a3ac251a8115d3c3de47a9418e63b28901c4f26a
jinsubin96 <jinsubin96@gmail.com>
2021-10-13 18:49:58 +0900

    test

---
+++ MOILSUROK2/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MOILSUROK2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "로그인을 먼저해주세요";
+
+            return View();
+        }
+        public ActionResult AddMember()
+        {
+
+
+            return View();
+        }
+        public ActionResult Member()
+        {
+           
+
+            return View();
+        }
+
+        public ActionResult AddNotic()
+        {
+
+
+            return View();
+        }
+        public ActionResult Notic()
+        {
+          
+
+            return View();
+        }
+
+        public ActionResult Calendar()
+        {
+
+            return View();
+        }
+        public ActionResult AddCalendar()
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ MOILSUROK2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MOILSUROK2.MvcApplication" Language="C#" %>
